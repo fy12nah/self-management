30fb108e6e3442d67d99b0e57dea28fe63c26c9e
Noora Al-Hassen <fy12nah@leeds.ac.uk>
2016-03-06 19:05:01 +0000

    changed textfields in landing view and java files to buttons

--- app/src/main/java/com/nooraalhassen/myapplication/LandingView.java
+++ app/src/main/java/com/nooraalhassen/myapplication/LandingView.java
@@ -5,14 +5,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LandingView extends AppCompatActivity {
 
-    TextView profile_tv;
-    TextView physical_tv;
-    TextView lifestyle_tv;
-    TextView illness_tv;
+    Button profile_tv;
+    Button physical_tv;
+    Button illness_tv;
+    Button meals_tv;
+    Button exer_tv;
+    Button sleep_tv;
+    Button mood_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +25,14 @@ public class LandingView extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        profile_tv = (TextView) findViewById(R.id.profile_tv);
-        physical_tv = (TextView) findViewById(R.id.physical_tv);
-        lifestyle_tv = (TextView) findViewById(R.id.lifes_tv);
-        illness_tv = (TextView) findViewById(R.id.illness_tv);
+        profile_tv = (Button) findViewById(R.id.landing_profile);
+        physical_tv = (Button) findViewById(R.id.landing_physical);
+        illness_tv = (Button) findViewById(R.id.landing_illness);
+        meals_tv = (Button) findViewById(R.id.landing_meals);
+        mood_tv = (Button) findViewById(R.id.landing_mood);
+        exer_tv = (Button) findViewById(R.id.landing_exer);
+        sleep_tv = (Button) findViewById(R.id.landing_sleep);
+
 
         // action - goes to Profile view when text is clicked
         profile_tv.setOnClickListener(new View.OnClickListener() {
@@ -46,15 +54,6 @@ public class LandingView extends AppCompatActivity {
             }
         });
 
-        // action - goes to Lifestyle view when text is clicked
-        lifestyle_tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // Intent intent = new Intent(LandingView.this, Lifestyle.class);
-               // startActivity(intent);
-            }
-        });
 
         // action - goes to Illness view when text is clicked
         illness_tv.setOnClickListener(new View.OnClickListener() {
@@ -63,6 +62,17 @@ public class LandingView extends AppCompatActivity {
 
                 // Intent intent = new Intent(LandingView.this, Illness.class);
                // startActivity(intent);
+            }
+        });
+
+
+        // action - goes to Lifestyle view when text is clicked
+        meals_tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Intent intent = new Intent(LandingView.this, Meals.class);
+                // startActivity(intent);
             }
         });
     }
